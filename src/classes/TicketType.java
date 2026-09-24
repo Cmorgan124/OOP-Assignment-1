ee3a23f0be2f3b45e3258168de9295bd6fb46582
rmoshajr <rmoshajr@gmail.com>
2026-09-24 01:35:50 -0500

    I made TicketType an immutable class with the getter functions getPrice and getTypeName.

--- src/classes/TicketType.java
+++ src/classes/TicketType.java
@@ -1,5 +1,32 @@
 package classes;
 
 public class TicketType {
-
+	private final String typeName;
+	private final double price;
+	
+	public TicketType(String typeName, double price)
+	{
+		if(typeName == null || typeName.isBlank())
+		{
+			throw new IllegalArgumentException("A name for the ticket type is required.");
+		}
+		if(price < 0)
+		{
+			throw new IllegalArgumentException("Price must be greater or equal to 0.");
+		}
+		this.typeName = typeName;
+		this.price = price;
+	}
+	
+	public String getTypeName()
+	{
+		return this.typeName;
+	}
+	
+	
+	public double getPrice()
+	{
+		return this.price;
+	}
+	
 }
