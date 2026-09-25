bf0b69921b6dce0d40de599deb96bf4ffabe6f5a
rmoshajr <306927121+rmoshajr@users.noreply.github.com>
2026-09-25 17:25:51 -0500

    feat: Add six private class fields to Ticket.java
    Created fields to store the required data for the ticket class.

--- src/classes/Ticket.java
+++ src/classes/Ticket.java
@@ -1,4 +1,10 @@
 package classes;
 
 public class Ticket {
+	private int id;
+	private String studentName;
+	private Event event;
+	private TicketType ticketType;
+	private boolean admitted;
+	private boolean canceled;
 }
