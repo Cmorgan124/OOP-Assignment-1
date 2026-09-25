74f037bc073a044cb90c9e8a5e66ef263f23e0d2
rmoshajr <306927121+rmoshajr@users.noreply.github.com>
2026-09-25 17:37:19 -0500

    feat:Add Ticket constructor validation and initialization
    Created a constructor that validates the required invariants for Ticket
    and sets the initial state of any new ticket object created.

--- src/classes/Ticket.java
+++ src/classes/Ticket.java
@@ -7,4 +7,40 @@ public class Ticket {
 	private TicketType ticketType;
 	private boolean admitted;
 	private boolean canceled;
+
+	
+	public Ticket(int id, String studentName, Event event, TicketType ticketType, boolean admitted, boolean canceled)
+	{
+		if(studentName == null || studentName.isBlank())
+		{
+			throw new IllegalArgumentException("A Student name for the ticket is required.");
+		}
+		if(id <= 0)
+		{
+			throw new IllegalArgumentException("ID number must be greater than 0.");
+
+		}
+		if (event == null)
+		{
+			throw new IllegalArgumentException("A ticket is required to have an event.");
+		}
+		if (ticketType == null)
+		{
+			throw new IllegalArgumentException("A ticket is required to have an ticket type.");
+		}
+		if(admitted)
+		{
+			canceled = false;
+		}
+		if(canceled)
+		{
+			admitted = false;
+		}
+		this.studentName = studentName;
+		this.id = id;
+		this.event = event;
+		this.ticketType = ticketType;
+		this.admitted = admitted;
+		this.canceled = canceled;
+	}
 }
