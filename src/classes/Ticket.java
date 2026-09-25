78366893ca2c66f4efb1d6ba12cbea7565eb1b52
rmoshajr <306927121+rmoshajr@users.noreply.github.com>
2026-09-25 17:52:10 -0500

    Fix admit() and cancel() base cases
    I updated each method's base case so that it changes the admitted or
    cancelled state to true before returning true. For admit(), I made sure
    that the admitted state would be true if the method was called and the
    cancelled state was false. For cancel(), I made sure that the canceled
    state would be true if the method was called and the admitted state was
    false.

--- src/classes/Ticket.java
+++ src/classes/Ticket.java
@@ -51,6 +51,7 @@ public class Ticket {
 			ticket.admitted = false;
 			return false;
 		}
+		ticket.admitted = true;
 		return true;
 	}
 
@@ -61,6 +62,7 @@ public class Ticket {
 			ticket.canceled = false;
 			return false;
 		}
+		ticket.canceled = true;
 		return true;
 	}
 
