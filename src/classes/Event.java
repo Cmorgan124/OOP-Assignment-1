6a0dd713859f600d02707364bf08e0cdccd3ce4a
Cmorgan124 <cdtm1245@gmail.com>
2026-09-25 01:43:10 -0500

    Filled out the Event class with all the required content.

--- src/classes/Event.java
+++ src/classes/Event.java
@@ -1,5 +1,36 @@
 package classes;
 
 public class Event {
-
+	private final String eventName;
+	private final String location;
+	
+	public Event(String eventName, String location)
+	{
+		if(eventName == null || eventName.isBlank()) 
+		{
+			throw new IllegalArgumentException("Name of event is required.");
+		}
+		if(location == null || location.isBlank()) 
+		{
+			throw new IllegalArgumentException("Location of event is required.");
+		}
+		this.eventName = eventName;
+		this.location = location;
+	}
+	
+	public String getEventName()
+	{
+		return this.eventName;
+	}
+	
+	
+	public String getLocation()
+	{
+		return this.location;
+	}
+	
+	public String toString() 
+	{
+		return this.eventName + "@" + this.location;
+	}
 }
