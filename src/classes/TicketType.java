b983876c734bbed286b0c419d59be2f77827543e
rmoshajr <306927121+rmoshajr@users.noreply.github.com>
2026-09-25 20:37:41 -0500

    Add ticketType fields
    Added two private final fields to store the required data and to make
    the class immutable.

--- src/classes/TicketType.java
+++ src/classes/TicketType.java
@@ -4,29 +4,4 @@ public class TicketType {
 	private final String typeName;
 	private final double price;
 	
-	public TicketType(String typeName, double price)
-	{
-		if(typeName == null || typeName.isBlank())
-		{
-			throw new IllegalArgumentException("A name for the ticket type is required.");
-		}
-		if(price < 0)
-		{
-			throw new IllegalArgumentException("Price must be greater or equal to 0.");
-		}
-		this.typeName = typeName;
-		this.price = price;
-	}
-	
-	public String getTypeName()
-	{
-		return this.typeName;
-	}
-	
-	
-	public double getPrice()
-	{
-		return this.price;
-	}
-	
 }
