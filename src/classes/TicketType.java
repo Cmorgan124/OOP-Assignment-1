d217c36cdd03970b0edf0ec51ac9fdf8452ad251
rmoshajr <306927121+rmoshajr@users.noreply.github.com>
2026-09-25 20:39:46 -0500

    Add TicketType invariant validation and initialization
    Added a constructor that validates the invariants for this class and
    then initializes the two values. Since this is an immutable class, this
    is the last and only time they will be initialized and set.

--- src/classes/TicketType.java
+++ src/classes/TicketType.java
@@ -4,4 +4,17 @@ public class TicketType {
 	private final String typeName;
 	private final double price;
 	
+	public TicketType(String typeName, double price)
+	{
+		if(typeName == null || typeName.isBlank())
+		{
+			throw new IllegalArgumentException("A name for the ticket type is required.");
+		}
+		if(price < 0)
+		{
+			throw new IllegalArgumentException("Price must be greater or equal to 0.");
+		}
+		this.typeName = typeName;
+		this.price = price;
+	}
 }
